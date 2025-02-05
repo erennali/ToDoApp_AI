68efd21036f49e1043e4b54642dcf4848a5ca6ed
Eren Ali Koca <eren_ali_koca@hotmail.com>
2025-02-05 13:32:19 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Eren Ali Koca on 5.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
